e4ee693fdb895c9a17c2139119883d7f2c07433e
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-03-01 23:36:15 -0800

    feat: selected answers are highlighted

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -14,51 +14,89 @@ const Title = styled.h1`
   margin: 2%;
 `;
 
-const Question = styled.div`
+const Question = styled.div``;
+
+const Answers = styled.div`
+  .answer {
+    width: 100%;
+    height: 20px;
+    padding: 2px;
+    margin: 2px;
+    background-color: ${props => props.selected ? 'orange' : '#ffeaea' };
+
+    &:hover {
+      background-color: ${props => props.selected ? 'orange' : '#fcffea' };
+    }
+  }
+
+  .answer_selected {
+    width: 100%;
+    height: 20px;
+    padding: 2px;
+    margin: 2px;
+    background-color: orange
+  }
 `;
 
-const Answers = styled.div``;
-
 const Answer = styled.div`
-  width:100%;
-  height:20px;
-  padding:2px;
-  margin:2px;
-  background-color: #ffeaea;
+  width: 100%;
+  height: 20px;
+  padding: 2px;
+  margin: 2px;
+  background-color: ${props => props.selected ? 'orange' : '#ffeaea' };
 
   &:hover {
-    background-color: #fcffea;
+    background-color: ${props => props.selected ? 'orange' : '#fcffea' };
   }
 `;
 
 const Quiz = (props) => {
   const { data } = props;
-  const [isAnswered, setIsAnswered] = useState(false);
+  const [selected, setSelected] = useState();
   const [questionIndex, setQuestionIndex] = useState(0);
 
-  const answers = data[questionIndex].answers;
+  const api = data[questionIndex];
+
+  const getClassName = (id) => {
+    return id = id === selected ? '_selected' : '';
+  }
+
+  const handleAnswer = (id) => {
+    selected === id ? setSelected(null) : setSelected(id);
+    // setSelected(!selected);
+    console.log(id);
+  }
+
+  const handleNext = () => {
+    if(questionIndex < data.length - 1){
+    setQuestionIndex(questionIndex + 1)
+    } else {
+      return <h1>no futher for you</h1>
+    }
+  }
 
   return (
     <StyledQuiz>
       <Title>QUIZ</Title>
-        <h5>Category: {data[questionIndex].category}</h5>
+      <h5>Category: {api.category}</h5>
       <Question>
-        <div>{data[questionIndex].question}</div>
+        <div>{api.question}</div>
       </Question>
       <Answers>
-      {/* {Object.keys(answers).map(answer => {
+        {/* Find a way to dry this code one day... */}
+        {/* {Object.keys(answers).map(answer => {
         return (
           <div>{answers}{answer}</div>
         )
       })} */}
-      {answers.answer_a === null ? null : <Answer>{answers.answer_a}</Answer>}
-      {answers.answer_b === null ? null : <Answer>{answers.answer_b}</Answer>}
-      {answers.answer_c === null ? null : <Answer>{answers.answer_c}</Answer>}
-      {answers.answer_d === null ? null : <Answer>{answers.answer_d}</Answer>}
-      {answers.answer_e === null ? null : <Answer>{answers.answer_e}</Answer>}
-      {answers.answer_f === null ? null : <Answer>{answers.answer_f}</Answer>}
+        {api.answers.answer_a && <div className={`answer${getClassName(1)}`} onClick={() => handleAnswer(1)} selected={selected}>{api.answers.answer_a}</div>}
+        {api.answers.answer_b && <div className={`answer${getClassName(2)}`} onClick={() => handleAnswer(2)} selected={selected}>{api.answers.answer_b}</div>}
+        {api.answers.answer_c && <div className={`answer${getClassName(3)}`} onClick={() => handleAnswer(3)} selected={selected}>{api.answers.answer_c}</div>}
+        {api.answers.answer_d && <div className={`answer${getClassName(4)}`} onClick={() => handleAnswer(4)} selected={selected}>{api.answers.answer_d}</div>}
+        {api.answers.answer_e && <div className={`answer${getClassName(5)}`} onClick={() => handleAnswer(5)} selected={selected}>{api.answers.answer_e}</div>}
+        {api.answers.answer_f && <div className={`answer${getClassName(6)}`} onClick={() => handleAnswer(6)} selected={selected}>{api.answers.answer_f}</div>}
       </Answers>
-        <button>next</button>
+      <button disabled={!selected} onClick={handleNext}>next</button>
     </StyledQuiz>
   );
 };
